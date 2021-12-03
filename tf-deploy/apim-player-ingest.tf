b5b604e403d6e07d3a486e643f6a7d52bc4215d7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-12-03 22:12:12 +0000

    Adding APIM API diagnostic logger

--- tf-deploy/apim-player-ingest.tf
+++ tf-deploy/apim-player-ingest.tf
@@ -16,10 +16,24 @@ resource "azurerm_api_management_api" "apim_player_ingest" {
   }
 }
 
-resource "azurerm_api_management_api_policy" "example" {
+resource "azurerm_api_management_api_policy" "player_ingest_api_policy" {
   api_name            = azurerm_api_management_api.apim_player_ingest.name
   api_management_name = azurerm_api_management.api_management.name
   resource_group_name = azurerm_resource_group.core_resource_group.name
 
   xml_content = replace(file("./policies/PlayerIngestPolicy.xml"), "__CODE__", data.azurerm_function_app_host_keys.function_app_host_key.default_function_key)
+}
+
+resource "azurerm_api_management_api_diagnostic" "player_ingest_api_diagnostic" {
+  identifier               = "applicationinsights"
+  resource_group_name      = azurerm_resource_group.core_resource_group.name
+  api_management_name      = azurerm_api_management.api_management.name
+  api_name                 = azurerm_api_management_api.apim_player_ingest.name
+  api_management_logger_id = azurerm_api_management_logger.api_management_api_logger.id
+
+  sampling_percentage       = 100
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "verbose"
+  http_correlation_protocol = "W3C"
 }
