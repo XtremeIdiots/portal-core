65afd6342e74aa9c549cc34cd2be5c6ae6dfc991
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-12-04 13:34:12 +0000

    Adding OnSay event structure

---
+++ src/common-lib/Models/OnSay.cs
@@ -0,0 +1,18 @@
+﻿namespace XtremeIdiots.Portal.CommonLib.Models
+{
+    public class OnSay
+    {
+        public OnSay(string username, string guid, string message, string type)
+        {
+            Username = username;
+            Guid = guid;
+            Message = message;
+            Type = type;
+        }
+
+        public string Username { get; set; }
+        public string Guid { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ src/function-app/GameEventsIngest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using XtremeIdiots.Portal.CommonLib.Models;
+
+namespace XtremeIdiots.Portal.FunctionApp
+{
+    public static class GameEventsIngest
+    {
+        [FunctionName("OnSay")]
+        [return: ServiceBus("chat_message_queue", Connection = "service-bus-connection-string")]
+        public static string OnSay([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] string input, ILogger log)
+        {
+            OnSay onSayEvent;
+            try
+            {
+                onSayEvent = JsonConvert.DeserializeObject<OnSay>(input);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "OnSay was not in expected format");
+                throw;
+            }
+
+            return JsonConvert.SerializeObject(onSayEvent);
+        }
+
+        [FunctionName("ProcessOnSay")]
+        public static void ProcessOnSay(
+        [ServiceBusTrigger("chat_message_queue", Connection = "service-bus-connection-string")] string myQueueItem, ILogger log)
+        {
+            OnSay onSayEvent;
+            try
+            {
+                onSayEvent = JsonConvert.DeserializeObject<OnSay>(myQueueItem);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "OnSay was not in expected format");
+                throw;
+            }
+
+            log.LogInformation($"ProcessOnSay :: Username: '{onSayEvent.Username}', Guid: '{onSayEvent.Guid}', Message: '{onSayEvent.Message}'");
+        }
+    }
+}
