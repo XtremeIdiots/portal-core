77f9826e45914be6e4c1aa6922fe089c76234f24
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-12-14 19:22:45 +0000

    Fixing references

--- tf-deploy/apim-player-ingest.tf
+++ tf-deploy/apim-player-ingest.tf
@@ -3,12 +3,12 @@ resource "azurerm_api_management_api" "apim_player_ingest" {
   resource_group_name = azurerm_resource_group.core_resource_group.name
   api_management_name = azurerm_api_management.api_management.name
 
-  service_url = format("https://%s/api/", azurerm_function_app.function_app.default_hostname)
+  service_url = format("https://%s/api/", azurerm_function_app.ingest_function_app.default_hostname)
 
-  revision            = "1"
-  display_name        = "Player Ingest"
-  path                = "player-ingest"
-  protocols           = ["https"]
+  revision     = "1"
+  display_name = "Player Ingest"
+  path         = "player-ingest"
+  protocols    = ["https"]
 
   import {
     content_format = "openapi+json"
@@ -21,7 +21,7 @@ resource "azurerm_api_management_api_policy" "player_ingest_api_policy" {
   api_management_name = azurerm_api_management.api_management.name
   resource_group_name = azurerm_resource_group.core_resource_group.name
 
-  xml_content = replace(file("./policies/PlayerIngestPolicy.xml"), "__CODE__", data.azurerm_function_app_host_keys.function_app_host_key.default_function_key)
+  xml_content = replace(file("./policies/PlayerIngestPolicy.xml"), "__CODE__", data.azurerm_function_app_host_keys.ingest_function_app_host_key.default_function_key)
 }
 
 resource "azurerm_api_management_api_diagnostic" "player_ingest_api_diagnostic" {
