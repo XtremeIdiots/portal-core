b9a507a1ac6ef33244abf75894e10827372c31f0
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-12-15 20:42:56 +0000

    Adding subscription state

--- tf-deploy/apim-subscriptions.tf
+++ tf-deploy/apim-subscriptions.tf
@@ -3,10 +3,12 @@ resource "azurerm_api_management_subscription" "b3bot_subscription" {
   resource_group_name = azurerm_api_management.api_management.resource_group_name
   display_name        = "B3Bots"
   api_id              = azurerm_api_management_api.apim_player_ingest.id
+  state               = "active"
 }
 
 resource "azurerm_api_management_subscription" "ingest_funcapp_subscription" {
   api_management_name = azurerm_api_management.api_management.name
   resource_group_name = azurerm_api_management.api_management.resource_group_name
   display_name        = "Ingest Function App"
+  state               = "active"
 }
