1d5b9ad1093e28b47dcbab3c2fe4dcab75d81729
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-12-17 21:57:12 +0000

    Adding chatlog clean up

--- src/function-apps/repository-func/ChatMessageRepository.cs
+++ src/function-apps/repository-func/ChatMessageRepository.cs
@@ -4,10 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using XtremeIdiots.Portal.DataLib;
 
---
+++ src/function-apps/repository-func/DataMaintenance.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using XtremeIdiots.Portal.DataLib;
+
+namespace XtremeIdiots.Portal.RepositoryFunc
+{
+    public class DataMaintenance
+    {
+        public PortalDbContext Context { get; }
+
+        public DataMaintenance(PortalDbContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [FunctionName("DataMaintenance")]
+        public void RunDataMaintenance([TimerTrigger("0 0 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"Performing Data Maintenance");
+
+            Context.Database.ExecuteSqlRawAsync($"DELETE FROM [dbo].[ChatMessages] WHERE [Timestamp] < CAST('{DateTime.UtcNow.AddMonths(-6):yyyy-MM-dd} 12:00:00' AS date)");
+        }
+    }
+}
