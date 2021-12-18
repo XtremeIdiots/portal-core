71bd5c24164d0e2e146c37baeb648f1f06f483e3
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-12-18 19:41:03 +0000

    Adding events funcapp backend

---
+++ tf-deploy/apim-backends.tf
@@ -0,0 +1,15 @@
+resource "azurerm_api_management_backend" "events_funcapp_backend" {
+  name                = local.ingest_function_app_name
+  resource_group_name = azurerm_resource_group.core_resource_group.name
+  api_management_name = azurerm_api_management.api_management.name
+
+  protocol    = "http"
+  url         = format("https://%s/api/", azurerm_function_app.events_function_app.default_hostname)
+  resource_id = azurerm_function_app.events_function_app.id
+
+  credentials {
+    query = {
+      "code" = data.azurerm_function_app_host_keys.events_function_app_host_key.default_function_key
+    }
+  }
+}
