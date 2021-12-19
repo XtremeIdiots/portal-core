d6326a23b8645fbba2da455121046fcda4ac2be3
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-12-19 15:56:05 +0000

    Adding repository web api backend to apim

--- tf-deploy/apim-backends.tf
+++ tf-deploy/apim-backends.tf
@@ -3,8 +3,8 @@ resource "azurerm_api_management_backend" "events_funcapp_backend" {
   resource_group_name = azurerm_resource_group.core_resource_group.name
   api_management_name = azurerm_api_management.api_management.name
 
-  protocol    = "http"
-  url         = format("https://%s/api/", azurerm_function_app.events_function_app.default_hostname)
+  protocol = "http"
+  url      = format("https://%s/api/", azurerm_function_app.events_function_app.default_hostname)
 
   credentials {
     query = {
@@ -18,12 +18,27 @@ resource "azurerm_api_management_backend" "repository_funcapp_backend" {
   resource_group_name = azurerm_resource_group.core_resource_group.name
   api_management_name = azurerm_api_management.api_management.name
 
-  protocol    = "http"
-  url         = format("https://%s/api/", azurerm_function_app.repository_function_app.default_hostname)
+  protocol = "http"
+  url      = format("https://%s/api/", azurerm_function_app.repository_function_app.default_hostname)
 
   credentials {
     query = {
       "code" = data.azurerm_function_app_host_keys.repository_function_app_host_key.master_key
     }
+  }
+}
+
+resource "azurerm_api_management_backend" "repository_webapi_backend" {
+  name                = azurerm_app_service.repository_web_api.name
+  resource_group_name = azurerm_resource_group.core_resource_group.name
+  api_management_name = azurerm_api_management.api_management.name
+
+  protocol = "http"
+  url      = format("https://%s/api/", azurerm_app_service.repository_web_api.default_site_hostname)
+
+  credentials {
+    certificate = [
+      azurerm_api_management_certificate.apim_repository_client_cert.thumbprint
+    ]
   }
 }
