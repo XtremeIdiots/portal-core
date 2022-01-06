2409572a8c88e19fa7d11ace1fde1ef98c4f355c
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-01-06 17:38:56 +0000

    Adding permission scopes

--- tf-deploy/repository-webapi.tf
+++ tf-deploy/repository-webapi.tf
@@ -1,8 +1,34 @@
+resource "random_uuid" "webapi_allaccess_uuid" {
+}
+
+resource "random_uuid" "webapi_gamesadmin_uuid" {
+}
+
 resource "azuread_application" "repository_webapi_application" {
   display_name     = local.repository_web_api_name
   owners           = [data.azuread_client_config.current.object_id]
   sign_in_audience = "AzureADMyOrg"
   identifier_uris  = [format("api://%s", local.repository_web_api_name)]
+
+  api {
+    oauth2_permission_scope {
+      admin_consent_description  = "AllAccess"
+      admin_consent_display_name = "AllAccess"
+      enabled                    = true
+      id                         = random_uuid.webapi_allaccess_uuid.result
+      type                       = "Admin"
+      value                      = "AllAccess"
+    }
+
+    oauth2_permission_scope {
+      admin_consent_description  = "GamesAdmin"
+      admin_consent_display_name = "GamesAdmin"
+      enabled                    = true
+      id                         = random_uuid.webapi_gamesadmin_uuid.result
+      type                       = "Admin"
+      value                      = "GamesAdmin"
+    }
+  }
 
   web {
     logout_url = format("https://%s.azurewebsites.net/signout-oidc", local.repository_web_api_name)
