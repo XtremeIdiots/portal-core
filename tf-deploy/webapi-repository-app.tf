19ddbac2d0422c879e278a0e28b6e7064551d867
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-01-06 20:39:23 +0000

    Guessing again

--- tf-deploy/webapi-repository-app.tf
+++ tf-deploy/webapi-repository-app.tf
@@ -65,12 +65,12 @@ resource "azuread_application" "repository_webapi_application" {
   }
 }
 
-data "azuread_service_principal" "repository_webapi_application_service_principal" {
-  display_name = local.repository_web_api_name
-}
+//data "azuread_service_principal" "repository_webapi_application_service_principal" {
+//  display_name = local.repository_web_api_name
+//}
 
 resource "azuread_app_role_assignment" "ingest_function_app_repository_role_assignment" {
   app_role_id         = random_uuid.webapi_serviceaccount_uuid.result
   principal_object_id = azurerm_function_app.ingest_function_app.identity[0].principal_id
-  resource_object_id  = data.azuread_service_principal.repository_webapi_application_service_principal.object_id
+  resource_object_id  = azuread_application.repository_webapi_application.object_id
 }
