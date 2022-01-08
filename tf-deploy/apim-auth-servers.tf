1462ef7346e5fbc570b16d05e4b3b893c1cc0475
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-01-08 11:31:18 +0000

    No idea what I am doing

---
+++ tf-deploy/apim-auth-servers.tf
@@ -0,0 +1,13 @@
+resource "azurerm_api_management_authorization_server" "events_function_app_application_auth_server" {
+  name                         = local.events_function_app_application_name
+  api_management_name          = azurerm_api_management.api_management.name
+  resource_group_name          = azurerm_api_management.api_management.resource_group_name
+  display_name                 = local.events_function_app_application_name
+  authorization_endpoint       = format("https://login.microsoftonline.com/%s/oauth2/v2.0/authorize", data.azurerm_client_config.current.tenant_id)
+  client_id                    = azuread_application.events_function_app_application.application_id
+  client_registration_endpoint = "https://login.microsoftonline.com"
+
+  grant_types = [
+    "authorizationCode",
+  ]
+}
