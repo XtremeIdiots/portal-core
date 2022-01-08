523c93ecaa75f5d4a7043683ed2b613b0569a288
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-01-08 12:19:39 +0000

    Adding required property

--- tf-deploy/apim-auth-servers.tf
+++ tf-deploy/apim-auth-servers.tf
@@ -5,9 +5,13 @@ resource "azurerm_api_management_authorization_server" "events_function_app_appl
   display_name                 = local.events_function_app_application_name
   authorization_methods        = ["GET", "POST"]
   authorization_endpoint       = format("https://login.microsoftonline.com/%s/oauth2/v2.0/authorize", data.azurerm_client_config.current.tenant_id)
-  token_endpoint               = format("https://login.microsoftonline.com/%s/oauth2/v2.0/token", data.azurerm_client_config.current.tenant_id) 
+  token_endpoint               = format("https://login.microsoftonline.com/%s/oauth2/v2.0/token", data.azurerm_client_config.current.tenant_id)
   client_id                    = azuread_application.events_function_app_application.application_id
   client_registration_endpoint = "https://login.microsoftonline.com"
+
+  bearer_token_sending_methods = [
+    "authorizationHeader"
+  ]
 
   grant_types = [
     "authorizationCode",
