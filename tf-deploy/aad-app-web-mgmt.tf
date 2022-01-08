5b932b4d1b45865d01912a3fbea58431915df6b8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-01-08 09:51:08 +0000

    Changing to application role

--- tf-deploy/aad-app-web-mgmt.tf
+++ tf-deploy/aad-app-web-mgmt.tf
@@ -1,21 +1,10 @@
-resource "random_uuid" "mgmt_web_app_access_uuid" {
+resource "random_uuid" "mgmt_web_app_application_user_uuid" {
 }
 
 resource "azuread_application" "mgmt_web_app_application" {
   display_name     = local.mgmt_web_app_application_name
   owners           = [data.azuread_client_config.current.object_id]
   sign_in_audience = "AzureADMyOrg"
-
-  api {
-    oauth2_permission_scope {
-      admin_consent_description  = "Access"
-      admin_consent_display_name = "Access"
-      enabled                    = true
-      id                         = random_uuid.mgmt_web_app_access_uuid.result
-      type                       = "Admin"
-      value                      = "Access"
-    }
-  }
 
   web {
     logout_url = format("https://%s.azurewebsites.net/signout-oidc", local.mgmt_web_app_name)
@@ -28,6 +17,15 @@ resource "azuread_application" "mgmt_web_app_application" {
       id_token_issuance_enabled     = true
     }
   }
+
+  app_role {
+    allowed_member_types = ["User"]
+    display_name         = "ApplicationUser"
+    description          = "Application users can access the management portal"
+    enabled              = true
+    id                   = random_uuid.mgmt_web_app_application_user_uuid.result
+    value                = "ApplicationUser"
+  }
 }
 
 resource "azuread_service_principal" "mgmt_web_app_local_service_principal" {
@@ -38,8 +36,8 @@ resource "azuread_service_principal" "mgmt_web_app_local_service_principal" {
   app_role_assignment_required = true
 }
 
-resource "azuread_app_role_assignment" "mgmt_web_app_access_scope_assignment" {
-  app_role_id         = random_uuid.mgmt_web_app_access_uuid.result
+resource "azuread_app_role_assignment" "mgmt_web_app_application_user_role_assignment" {
+  app_role_id         = random_uuid.mgmt_web_app_application_user_uuid.result
   principal_object_id = azuread_group.mgmt_web_app_users_group.object_id
   resource_object_id  = azuread_service_principal.mgmt_web_app_local_service_principal.object_id
 }
