9f1e46015f6edc63340fbe1a14ca506d65cc1c6a
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-01-13 20:39:16 +0000

    Trying different syntax in script

--- src/database/Scripts/Script.PostDeploymentAccounts.sql
+++ src/database/Scripts/Script.PostDeploymentAccounts.sql
@@ -10,9 +10,9 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-IF (NOT EXISTS(SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-portal-$(env)-eastus-01-admins'))  
+IF (NOT EXISTS(SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-portal-' + $(env) + '-eastus-01-admins'))  
 BEGIN  
 	CREATE USER [sg-sql-portal-box-eastus-01-admins] FROM EXTERNAL PROVIDER
 	ALTER ROLE [db_datawriter] ADD MEMBER [sg-sql-portal-$(env)-eastus-01-admins]
-	SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-portal-$(env)-eastus-01-admins'
+	SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-portal-' + $(env) + '-eastus-01-admins'
 END  
