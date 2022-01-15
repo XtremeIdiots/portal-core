044fedfc37f450f159e75c1853427e14a4d67907
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-01-15 22:07:29 +0000

    Removing role assignment as would need to change principal permissions

--- tf-deploy/aad-role-assignments.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "sql_server_principal_directory_reader_role" {
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = "Directory Readers"
-  principal_id         = azurerm_mssql_server.sql_server.identity[0].principal_id
-}
