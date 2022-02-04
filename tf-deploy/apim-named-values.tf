7a7cca79815802d42c8dac2f238ab379a773b125
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-02-04 17:36:17 +0000

    Set named values as secrets

--- tf-deploy/apim-named-values.tf
+++ tf-deploy/apim-named-values.tf
@@ -3,6 +3,7 @@ resource "azurerm_api_management_named_value" "events_function_app_key" {
   resource_group_name = azurerm_resource_group.core_resource_group.name
   api_management_name = azurerm_api_management.api_management.name
   display_name        = "Events Function App Key"
+  secret              = true
 
   value_from_key_vault {
     secret_id = azurerm_key_vault_secret.events_function_app_key.versionless_id
@@ -14,6 +15,7 @@ resource "azurerm_api_management_named_value" "repository_function_app_key" {
   resource_group_name = azurerm_resource_group.core_resource_group.name
   api_management_name = azurerm_api_management.api_management.name
   display_name        = "Repository Function App Key"
+  secret              = true
 
   value_from_key_vault {
     secret_id = azurerm_key_vault_secret.repository_function_app_key.versionless_id
